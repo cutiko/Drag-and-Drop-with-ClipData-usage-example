15dfd87ebe253df8a0f8069396cb731ed21d0950
Cutiko <cutiko@gmail.com>
2015-11-08 18:54:02 -0300

    functional example

---
+++ app/src/main/java/cl/cutiko/dragdrop/MainActivity.java
@@ -0,0 +1,100 @@
+package cl.cutiko.dragdrop;
+
+import android.content.ClipData;
+import android.content.ClipDescription;
+import android.content.ClipboardManager;
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.DragEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String tag;
+
+    private ClipboardManager clipboardManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        clipboardManager = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+
+        findViewById(R.id.myimage1).setOnTouchListener(new MyTouchListener());
+        findViewById(R.id.myimage2).setOnTouchListener(new MyTouchListener());
+        findViewById(R.id.myimage3).setOnTouchListener(new MyTouchListener());
+        findViewById(R.id.myimage4).setOnTouchListener(new MyTouchListener());
+        findViewById(R.id.topleft).setOnDragListener(new MyDragListener());
+        findViewById(R.id.topright).setOnDragListener(new MyDragListener());
+        findViewById(R.id.bottomleft).setOnDragListener(new MyDragListener());
+        findViewById(R.id.bottomright).setOnDragListener(new MyDragListener());
+    }
+
+    private final class MyTouchListener implements View.OnTouchListener {
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                ClipData clipData = ClipData.newPlainText("tag", view.getTag().toString());
+                //clipboardManager.setPrimaryClip(clipData);
+
+                View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+                view.startDrag(clipData, shadowBuilder, view, 0);
+                view.setVisibility(View.INVISIBLE);
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    class MyDragListener implements View.OnDragListener {
+        Drawable enterShape = getResources().getDrawable(R.drawable.shape_drop_target);
+        Drawable normalShape = getResources().getDrawable(R.drawable.shape);
+
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+            int action = event.getAction();
+            switch (event.getAction()) {
+                case DragEvent.ACTION_DRAG_STARTED:
+                    break;
+                case DragEvent.ACTION_DRAG_ENTERED:
+                    v.setBackgroundDrawable(enterShape);
+                    break;
+                case DragEvent.ACTION_DRAG_EXITED:
+                    v.setBackgroundDrawable(normalShape);
+                    break;
+                case DragEvent.ACTION_DROP:
+                    // Dropped, reassign View to ViewGroup
+                    View view = (View) event.getLocalState();
+                    ViewGroup owner = (ViewGroup) view.getParent();
+                    owner.removeView(view);
+                    LinearLayout container = (LinearLayout) v;
+                    container.addView(view);
+                    view.setVisibility(View.VISIBLE);
+                    //event.getClipDescription().getLabel().toString()
+                    //event.getClipData().toString()
+                    ClipData.Item item = event.getClipData().getItemAt(0);
+                    Toast.makeText(getApplicationContext(), item.getText().toString(), Toast.LENGTH_SHORT).show();
+                    break;
+                case DragEvent.ACTION_DRAG_ENDED:
+                    v.setBackgroundDrawable(normalShape);
+                    /*ClipData clipData = clipboardManager.getPrimaryClip();
+                    ClipData.Item item = clipData.getItemAt(0);
+                    String text = item.getText().toString();*/
+                    //event.getClipDescription().getLabel().equals("tag");
+                default:
+                    break;
+            }
+            return true;
+        }
+    }
+
+
+}
